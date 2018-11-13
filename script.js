71bfcc49333449dde702deaec525eaac675361c6
GJVA <gertjan.vanlook@gmail.com>
2018-11-13 22:15:10 +0100

    adding features test

---
+++ script.js
@@ -0,0 +1,26 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+$(document).ready(function(){ 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 6000);
+
+});
+
+
